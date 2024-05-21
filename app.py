5cb2cb5dd55e9597b5375968e3e15f774057bf0a
JimenaMaSuMu <jimenaszmz@gmail.com>
2024-05-21 00:24:20 -0600

    update of graphs test

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ st.header('Lanzar una moneda')
 st.write('Esta aplicación aún no es funcional. En construcción.')
 
 
-car_data = pd.read_csv('datasets\cars_us.csv') # leer los datos
+car_data = pd.read_csv('datasets/cars_us.csv') # leer los datos
 fig = px.histogram(car_data, x="odometer") # crear un histograma
 fig.show() # crear gráfico de dispersión
 
