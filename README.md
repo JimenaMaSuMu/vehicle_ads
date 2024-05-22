3f97f2f44eb3ce4be558e2c4a206036444d6005a
JimenaMaSuMu <jimenaszmz@gmail.com>
2024-05-21 19:14:26 -0600

    last updates for the project

--- README.md
+++ README.md
@@ -1,10 +1,15 @@
 # vehicle_ads
 Proyecto de anuncios de vehiculos. 
-Podemos observar dos diferentes graficos que representan los precios de diferentes
+Podemos observar cinco diferentes graficos que representan los precios, condiciones y año modelo de diferentes
 marcas de carros.
-La primera es el histograma de los preciosde todos los carros almacenados.
-La segunda es la disperción de los precios de todos los carros almacenados.
-En la aplicación puedes seleccionar con el checkbox la grafica deseada. 
+Para visualizar cada grafico se utilizaron checkboxes:
+La primera es el histograma de los precios de todos los carros almacenados.
+La segunda es el histograma de los años de todos los carros almacenados.
+La tercera es el histograma de las condiciones en las que se encuentras de todos los carros almacenados.
+La cuarta es un diagrama de disperción de los precios contra el año modelo de todos los carros almacenados.
+La quinta es un diagrama de disperción de los precios contra la condición en la que están todos los carros almacenados.
 
-No utilicé la configuracion de 'config.toml' porque me funcionó a la perfeccion sin especificarlo.
+
+No utilicé la configuracion de 'config.toml' para ocupar un puerto en especifico porque me funcionó a la perfeccion sin especificarlo.
+Mas bien agregué colores de fondo y colores de letra.
 
--- app.py
+++ app.py
@@ -16,34 +16,39 @@ st.write('Selecciona el grafico deseado.')
 
 car_data = pd.read_csv('datasets/cars_us.csv') # leer los datos
 #hist_button = st.button('Construir histograma')
-hist_check = st.checkbox('Construir histograma de precios')
-if hist_check: # al hacer clic en el botón
+hist_check_price = st.checkbox('Construir histograma de precios')
+if hist_check_price: # al hacer clic en el botón
     # escribir un mensaje
-    st.write('Creación de un histograma para el conjunto de datos de los precios de coches')    
+    st.write('Histograma para el conjunto de datos de los precios de coches')    
     # crear un histograma
-    fig = px.histogram(car_data, x="price")
+    fig_p = px.histogram(car_data, x="price")
     # mostrar un gráfico Plotly interactivo
-    st.plotly_chart(fig, use_container_width=True)
+    st.plotly_chart(fig_p, use_container_width=True)
+
+hist_check_year = st.checkbox('Construir histograma enfocando a los años de los coches')
+if hist_check_year:
+    st.write('Histograma para el conjunto de datos de los años de los coches')
+    fig_y = px.histogram(car_data , x='model_year')
+    st.plotly_chart(fig_y, use_container_width=True)
+
+hist_check_condition = st.checkbox('Construir histograma enfocando a la condicion de los coches')
+if hist_check_condition:
+    st.write('Histograma para el conjunto de datos de la condicion de cada coche')
+    fig_c = px.histogram(car_data , x='condition')
+    st.plotly_chart(fig_c, use_container_width=True)
 
 #scatter_button = st.button('Construir diagrama de dispersion')
-scatter_check = st.checkbox('Construir diagrama de dispersion de precios')
+scatter_check = st.checkbox('Construir diagrama de dispersion de precios vs. año modelo')
 if scatter_check: # al hacer clic en el botón
     # escribir un mensaje
-    st.write('Creación de un diagrama de dispersion para el conjunto de datos los precios de venta de coches')    
+    st.write('Diagrama de dispersion para el conjunto de datos los precios con respecto al año modelo de los coches')    
     # crear un histograma
-    fig1 = px.scatter(car_data, x="price")
+    fig1 = px.scatter(car_data, y="price",x='model_year')
     # mostrar un gráfico Plotly interactivo
     st.plotly_chart(fig1, use_container_width=True)
 
-
-hist_check_brand = st.checkbox('Construir histograma enfocando a las marcas de los coches')
-if hist_check_brand:
-    st.write('Creación de un histograma para el conjunto de datos de las marcas de coches')
-    fig2 = px.histogram(car_data , x='model')
-    st.plotly_chart(fig2, use_container_width=True)
-
-scatter_check_brand = st.checkbox('Construir diagrama de dispersion de las marcas')
-if hist_check_brand:
-    st.write('Construir diagrama de dispersion enfocando a las marcas de los coches')
-    fig2 = px.histogram(car_data , x='model')
-    st.plotly_chart(fig2, use_container_width=True)
+scatter_check_p_c = st.checkbox('Construir diagrama de dispersion comparando precio y condicion de los coches')
+if scatter_check_p_c:
+    st.write('Diagrama de dispersion comparando el año de los coches y su condicion')
+    fig_p_c = px.scatter(car_data , y='price',x='condition')
+    st.plotly_chart(fig_p_c, use_container_width=True)
